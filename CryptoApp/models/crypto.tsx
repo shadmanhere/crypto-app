73dbf3347deb4e2b72a435b0fe5dcad27869ece5
Shadman Ali <shadman.ali@live.co.uk>
2023-08-17 09:01:55 +0530

    add crypto list on home screen

---
+++ CryptoApp/models/crypto.tsx
@@ -0,0 +1,5 @@
+export interface Crypto {
+  id: string;
+  name: string;
+  price: number;
+}
--- CryptoApp/screens/Home.tsx
+++ CryptoApp/screens/Home.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import {Crypto} from '../models/crypto';
 
-export const HomeScreen = ({navigation}: {navigation: any}) => {
-  const navigateToDetail = () => {
-    navigation.navigate('Detail');
-  };
+const cryptoList: Crypto[] = [
+  {
+    id: '1',
+    name: 'BTC',
+    price: 3800.64,
+  },
+  {
+    id: '2',
+    name: 'ETH',
+    price: 4000.0,
+  },
+  {
+    id: '3',
+    name: 'SQL',
+    price: 250.21,
+  },
+];
+
+const renderItem = ({item}: {item: Crypto}) => {
+  return <Text>{item.name}</Text>;
+};
+
+export const HomeScreen = ({}: {navigation: any}) => {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Pressable onPress={navigateToDetail}>
-        <Text>Navigate to Detail</Text>
-      </Pressable>
+      <FlatList
+        data={cryptoList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };

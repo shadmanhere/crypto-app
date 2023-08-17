d2200d07cb740cbdfdbf55154942010aef7985c9
Shadman Ali <shadman.ali@live.co.uk>
2023-08-17 09:53:06 +0530

    add socket io and dotenv

--- CryptoApp/App.tsx
+++ CryptoApp/App.tsx
@@ -8,10 +8,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {API_URL} from '@env';
 
 import {HomeScreen, DetailScreen} from './screens';
+import io from 'socket.io-client';
 
 const Stack = createNativeStackNavigator();
+const socket = io(API_URL);
+
+socket.on('connect', () => {
+  console.log('socket connected');
+});
 
 const App = () => {
   return (
